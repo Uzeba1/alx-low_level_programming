daee85d2b14acb10807f7909b813b2c0ac61b1ea
Uzeba1 <uzebag@gmail.com>
2022-10-31 19:20:28 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - fill a block of memory with a specify value
+ * @s: starting address of memory to be filled
+ * @b: the desired value for n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+my c pointers and arrays
